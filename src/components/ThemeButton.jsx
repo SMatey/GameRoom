afab07d32da150c0084c7dd675bc78d03a4b5976
Sebastian Matey <casamarosebas@gmail.com>
2025-07-09 16:50:42 -0600

    Refactor theme logic in ThemeButton and remove ThemeProvider
    Simplified theme management by handling theme state and persistence directly within ThemeButton.jsx. Removed the unused ThemeProvider.jsx file.

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -4,47 +4,29 @@ import { SunIcon, MoonIcon } from "./Icons";
 export const ThemeButton = () => {
     const [theme, setTheme] = useState('light');
 
+    const toggleTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+        document.documentElement.classList.toggle('dark', newTheme === 'dark');
+        localStorage.setItem('theme', newTheme);
+    };
+
     // Cambiar tema
     useEffect(() => {
-    const savedTheme = localStorage.getItem('theme');
-    const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    
-    if (savedTheme) {
-        setTheme(savedTheme);
-    }else if (systemPrefersDark) {
-        setTheme('dark');
-    } else {
-        setTheme('light');
-    }
+        const storagedTheme = localStorage.getItem("theme") || "light";
+        setTheme(storagedTheme);
+        document.documentElement.classList.toggle('dark', storagedTheme === 'dark');
     }, []);
-
-
-    useEffect(() => {
-        const html = document.documentElement;
-        if (theme === 'dark') {
-            html.classList.add('dark');
-        } else {
-            html.classList.remove('dark');
-        }
-        localStorage.setItem('theme', theme);
-    }, [theme]);
-
-    const toggleTheme = () => {
-        setTheme(theme === 'light' ? 'dark' : 'light');
-    };
 
 
     return (
         <div>
             <button
             onClick={toggleTheme}
-            aria-label={`Switch to ${theme === 'dark' ? 'light' : 'dark'} mode`}
-            className="rounded-xl p-2 bg-surface-light dark:bg-surface border border-bprimary-light  hover:bg-bprimary-light/10 dark:hover:bg-bprimary/10 transition-colors"
+            className="rounded-xl p-2 bg-surface-light dark:bg-surface border border-bprimary-light  hover:bg-bprimary-light/10 dark:hover:bg-bprimary/10 transition-colors cursor-pointer"
             >
             {
-                theme === 'dark' 
-                ? <SunIcon className="w-6 h-6 text-bprimary stroke-current" />
-                : <MoonIcon className="text-bprimary-light stroke-current w-6 h-6" /> 
+                theme === 'light' ? <MoonIcon /> : <SunIcon />
             }
             </button>
         </div>
