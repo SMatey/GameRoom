b31b089abe163e4ab722ca7afcc8976d43d6eadf
Sebastian Matey <casamarosebas@gmail.com>
2025-07-31 17:18:11 -0600

    Add favorites retrieval to FavoritesPage
    Imported and called getFavorites in FavoritesPage to retrieve the list of favorite items. Prepared an empty list for displaying favorites, but rendering of items is not yet implemented.

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,8 +1,14 @@
+import { getFavorites } from "../services/favoritesService";
+
 const FavoritesPage = () => {
+  const favorites = getFavorites();
+
   return (
     <div>
       <h1>Favorites</h1>
       <p>Here you can find your favorite items.</p>
+      <ul>
+      </ul>
     </div>
   );
 };
